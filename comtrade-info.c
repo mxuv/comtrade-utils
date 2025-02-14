d948b86ccb3e06070601bf9e6b85dfcefd6c034d
Maxx UV <mxuv dot dev at gamil dot com>
2025-02-14 15:22:58 +0300

    getstring

--- comtrade-info.c
+++ comtrade-info.c
@@ -1,11 +1,53 @@
 #include <stdio.h>
 
+#include "strings.h"
 #include "format.h"
 
+#define STR_BUFSIZE             4096
+
+enum getstring_status
+{
+    gls_ok,
+    gls_empty,
+    gls_eof,
+    gls_err,
+    gls_overflow
+};
+
+int getstring(FILE *fd, char *buffer, int size, enum getstring_status *status)
+{
+    int len = 0;
+
+    *status = gls_ok;
+    fgets(buffer, size, fd);
+    len = stringlen(buffer);
+    if (len == 0)
+        *status = gls_empty;
+    if (buffer[len-1] != '\n') {
+        if (feof(fd))
+            *status = gls_eof;
+        if (ferror(fd))
+            *status = gls_err;
+        if (len == size - 1)
+            *status = gls_overflow;
+    }
+    return len;
+}
+
+void read_header()
+{
+}
+
+void analyze_cfgfile(FILE *fd)
+{
+    char buffer[STR_BUFSIZE];
+}
 
 int main(int argc, char **argv)
 {
     FILE *fd;
+    enum getstring_status status;
+    char buffer[4096];
     cmtrd_cfg_body_t cfg_body;
 
     if (argc < 2) {
@@ -19,7 +61,32 @@ int main(int argc, char **argv)
         perror(argv[1]);
         return 1;
     }
-
     fclose(fd);
     return 0;
 } 
+
+
+/*
+    int len = getstring(fd, buffer, 4094, &status);
+    switch (status) {
+    case gls_ok:
+        printf(buffer);
+        printf("%d\n", len);
+        break;
+    case gls_empty:
+        fputs("Empty string\n", stderr);
+        break;
+    case gls_eof:
+        fputs("End of file\n", stderr);
+        break;
+    case gls_err:
+        perror(argv[1]);
+        break;
+    case gls_overflow:
+        fputs("Buffer overflow\n", stderr);
+        break;
+    default:
+        break;
+    }
+
+*/
