9a2a5bf6f1835cf3be1e646b8c0f1474aa264feb
Maxx UV <mxuv dot dev at gamil dot com>
2025-02-26 10:21:49 +0300

    Add string messages
    Add messages of line errors, add messages of parametrs name

--- comtrade-info.c
+++ comtrade-info.c
@@ -49,15 +49,15 @@ const char lnerrmsg1[] = "Line contains extra spaces";
 const char lnerrmsg2[] = "Line contains too many parametrs";
 const char lnerrmsg3[] = "Line contains too few parametrs";
 const char lnerrmsg4[] = "Incorrect parameter";
-const char lnerrmsg5[] = "Incorrect parameter length"
+const char lnerrmsg5[] = "Incorrect parameter length";
 
 const char parammsg0[] = "Station name";
 const char parammsg1[] = "Recording device id";
 const char parammsg2[] = "Year of the standart revision";
 const char parammsg3[] = "Total channels count";
 const char parammsg4[] = "Analog channels count";
-const char parammsg5[] = "Binary channels count";
-const char parammsg6[] = "Channel number";
+const char parammsg5[] = "Digital channels count";
+const char parammsg6[] = "Analog channel number";
 const char parammsg7[] = "Channel id";
 const char parammsg8[] = "Phase id";
 const char parammsg9[] = "Circuit component";
@@ -66,28 +66,39 @@ const char parammsg11[] = "Channel multipler";
 const char parammsg12[] = "Channel offset";
 const char parammsg13[] = "Channel time skew";
 const char parammsg14[] = "Channel minimum data value";
-const char parammsg15[] = "Channel data value";
-const char parammsg16[] = "";
-const char parammsg17[] = "";
-const char parammsg18[] = "";
-const char parammsg19[] = "";
-const char parammsg20[] = "";
-const char parammsg21[] = "";
-const char parammsg22[] = "";
-const char parammsg23[] = "";
-const char parammsg24[] = "";
-const char parammsg25[] = "";
-const char parammsg26[] = "";
-const char parammsg27[] = "";
-const char parammsg28[] = "";
-const char parammsg29[] = "";
-const char parammsg30[] = "";
-const char parammsg31[] = "";
-const char parammsg32[] = "";
-const char parammsg33[] = "";
-const char parammsg34[] = "";
-const char parammsg35[] = "";
-const char parammsg36[] = "";
+const char parammsg15[] = "Channel maximum data value";
+const char parammsg16[] = "Channel transformer ratio primary factor";
+const char parammsg17[] = "Channel transformer ratio secondary factor";
+const char parammsg18[] = "Channel primary or secondary data scalling indentifier;";
+const char parammsg19[] = "Digital channel number";
+const char parammsg20[] = "Channel normal state";
+const char parammsg21[] = "Line frequency";
+const char parammsg22[] = "Number of sampling rates";
+const char parammsg23[] = "Sample rate";
+const char parammsg24[] = "Last sample number";
+const char parammsg25[] = "Day";
+const char parammsg26[] = "Month";
+const char parammsg27[] = "Year";
+const char parammsg28[] = "Hour";
+const char parammsg29[] = "Minuts";
+const char parammsg30[] = "Seconds";
+const char parammsg31[] = "Data file type";
+const char parammsg32[] = "Time multiplicaation factor";
+const char parammsg33[] = "Time code";
+const char parammsg34[] = "Local time code";
+const char parammsg35[] = "Time quality code";
+const char parammsg36[] = "Leap second indicator";
+
+const char *lnerrmsg[] = { lnerrmsg0, lnerrmsg1, lnerrmsg2, lnerrmsg3,
+        lnerrmsg4, lnerrmsg5 };
+
+const char *parammsg[] = { parammsg0, parammsg1, parammsg2, parammsg3,
+        parammsg4, parammsg5, parammsg6, parammsg7, parammsg8, parammsg9,
+        parammsg10, parammsg11, parammsg12, parammsg13, parammsg14, parammsg15,
+        parammsg16, parammsg17, parammsg18, parammsg19, parammsg20, parammsg21,
+        parammsg22, parammsg23, parammsg24, parammsg25, parammsg26, parammsg27,
+        parammsg28, parammsg29, parammsg30, parammsg31, parammsg32, parammsg33, 
+        parammsg34, parammsg35, parammsg36 }; 
 
 int match_char(char ch, char patt)
 {
@@ -359,7 +370,7 @@ void cfg_record_init(cmtrd_cfg_body_t *cfg_rec)
 void print_errors(cmtrd_cfg_body_t *cfg_rec)
 {
     int i;
-    
+
     for (i = 0; i < cfg_rec->errcount; i++) {
 
     }
