41042f64ad819be88d54c9aa5c78fdbf7c1cdd0e
Maxim UV <Maximumuvm at gmail dot com>
2026-02-16 23:38:45 +0300

    Create format const

---
+++ format_const.c
@@ -0,0 +1,49 @@
+#include "format_const.h"
+
+const int tv_revyear_min        = 1991;
+const int tv_revyear_max        = 2013;
+
+const int tv_tt_min             = 1;
+const int tv_tt_max             = 999999;
+const int tv_an_min             = 0;
+const int tv_an_max             = 999999;
+const int tv_dn_min             = 0;
+const int tv_dn_max             = 999999;
+
+const int tv_a_n_min            = 1;
+const int tv_a_n_max            = 999999;
+const double tv_a_minsc_min     = -3.4028235E38;
+const double tv_a_minsc_max     = 3.4028235E38;
+const double tv_a_maxsc_min     = -3.4028235E38;
+const double tv_a_maxsc_max     = 3.4028235E38;
+
+const int tv_d_n_min            = 1;
+const int tv_d_n_max            = 999999;
+const int tv_d_y_min            = 0;
+const int tv_d_y_max            = 1;
+
+const int tv_nrates_min         = 0;
+const int tv_nrates_max         = 999;
+const long long tv_endsamp_min  = 1;
+const long long tv_endsamp_max  = 99999999999;
+
+const int tv_day_min            = 1;
+const int tv_day_max            = 31;
+const int tv_mon_min            = 1;
+const int tv_mon_max            = 12;
+const int tv_year_min           = 1900;
+const int tv_year_max           = 9999;
+const int tv_hour_min           = 0;
+const int tv_hour_max           = 23;
+const int tv_minute_min         = 0;
+const int tv_minute_max         = 59;
+const int tv_seconds_p_min      = 0;
+const int tv_seconds_p_max      = 59;
+const long long tv_seconds_s_min= 0;
+const long long tv_seconds_s_max= 99999999999;
+
+const int tv_tmq_min            = 0;
+const int tv_tmq_max            = 15;
+
+const int tv_leapsec_min        = 0;
+const int tv_leapsec_max        = 3;
---
+++ format_const.h
@@ -0,0 +1,52 @@
+#ifndef __FORMAT_CONST_H__
+#define __FORMAT_CONST_H__
+
+extern const int tv_revyear_min;
+extern const int tv_revyear_max;
+
+extern const int tv_tt_min;
+extern const int tv_tt_max;
+extern const int tv_an_min;
+extern const int tv_an_max;
+extern const int tv_dn_min;
+extern const int tv_dn_max;
+
+extern const int tv_a_n_min;
+extern const int tv_a_n_max;
+extern const double tv_a_minsc_min;
+extern const double tv_a_minsc_max;
+extern const double tv_a_maxsc_min;
+extern const double tv_a_maxsc_max;
+
+extern const int tv_d_n_min;
+extern const int tv_d_n_max;
+extern const int tv_d_y_min;
+extern const int tv_d_y_max;
+
+extern const int tv_nrates_min;
+extern const int tv_nrates_max;
+extern const long long tv_endsamp_min;
+extern const long long tv_endsamp_max;
+
+extern const int tv_day_min;
+extern const int tv_day_max;
+extern const int tv_mon_min;
+extern const int tv_mon_max;
+extern const int tv_year_min;
+extern const int tv_year_max;
+extern const int tv_hour_min;
+extern const int tv_hour_max;
+extern const int tv_minute_min;
+extern const int tv_minute_max;
+extern const int tv_seconds_p_min;
+extern const int tv_seconds_p_max;
+extern const long long tv_seconds_s_min;
+extern const long long tv_seconds_s_max;
+
+extern const int tv_tmq_min;
+extern const int tv_tmq_max;
+
+extern const int tv_leapsec_min;
+extern const int tv_leapsec_max;
+
+#endif
